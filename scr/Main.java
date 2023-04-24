7ba0cc32399f6442f52075bea5b5bcea6a1cd1c4
AutomationTry <susan.help.test@gmail.com>
2023-04-24 23:46:48 +0300

    final changes

--- scr/Main.java
+++ scr/Main.java
@@ -4,46 +4,53 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        String[] words = new String[]{"mama", "tato", "and new me"};
+        String[] words = new String[]{"Hello", "mama", "papa", "its me"};
         pairedLetters(words);
     }
 
     public static void pairedLetters(String[] words) {
-
         List<String> helpList = new ArrayList<>();
+        int count = 0;
 
-        for(String word: words) {
-        char [] characters = word.toCharArray();
+        for (int i = 0; i < words.length; i++) {
 
-            Map<Character, Integer> helpMap = new HashMap<>();
-            for(Character character : characters) {
-                if(helpMap.containsKey(character)) {
-                Integer value = helpMap.get(character)+1;
-                helpMap.put(character, value);
-                } else{
-                    helpMap.put(character, 1);
-                }
+            String word = words[i];
+
+            while (count == 2) {
+                System.out.println(helpList);
+                break;
             }
+            char[] characters = word.toCharArray();
 
-            boolean isRightWord = true;
-
-            for (Character key: helpMap.keySet()) {
-                if (helpMap.get(key) % 2 != 0) {
-                    isRightWord = false;
+            Map<Character, Integer> charMap = new HashMap<>();
+            for (Character character : characters) {
+                if (charMap.containsKey(character)) {
+                    Integer value = charMap.get(character) + 1;
+                    charMap.put(character, value);
+                } else {
+                    charMap.put(character, 1);
                 }
-            }
-            if(isRightWord) {
-                helpList.add(word);
+
+                boolean isRightWord = true;
+
+                for (Character key : charMap.keySet()) {
+                    if (charMap.get(key) % 2 != 0) {
+                        isRightWord = false;
+                    }
+                }
+                if (isRightWord) {
+                    helpList.add(word);
+                    count++;
+                }
             }
         }
 
-        Set<Character> mySet = new HashSet<>();
-
-        for (String chars: helpList) {
-            for (Character character : chars.toCharArray()) {
-                mySet.add(character);
-
+            Set<Character> uniqueLetters = new HashSet<>();
+            for (String uniqueLetter : helpList) {
+                for (Character letter : uniqueLetter.toCharArray()) {
+                    uniqueLetters.add(letter);
+                }
+                System.out.println(uniqueLetters);
             }
         }
     }
-}
